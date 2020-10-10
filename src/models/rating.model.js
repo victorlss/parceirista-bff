ec595662af8b2e2b373979c1e9f28f042f2718cb
victorlss <victor.souza@walmart.com>
2020-10-10 15:41:14 -0300

    Add rating model

--- src/models/rating.model.js
+++ src/models/rating.model.js
@@ -1,8 +1,10 @@
-export default class Rating {
-    ratingId;
-    contractId;
-    bussinessId;
-    professionalId;
-    rating;
-    createdAt;
-}
+const mongoose = require('mongoose')
+
+const ratingSchema = new mongoose.Schema({
+  contractId: { type: String },
+  businessId: { type: String },
+  professionalId: { type: String },
+  rating: { type: Number }
+}, { timestamps: true })
+
+module.exports = mongoose.model('rating', ratingSchema, 'ratings')
