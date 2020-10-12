a7d99e84c43484b1f524fc2591e14c14fa1aae7e
victorlss <victor.souza@walmart.com>
2020-10-12 19:58:30 -0300

    CHange endpoint get services by professionalId

--- src/app/service/routes.js
+++ src/app/service/routes.js
@@ -12,7 +12,7 @@ router.post('/', async (request, response, next) => {
   await resolver.handle(response, next, async () => await controller.createService(request.body))
 })
 
-router.get('/user/:id', async (request, response, next) => {
+router.get('/professional/:id', async (request, response, next) => {
   await resolver.handle(response, next, async () => controller.getByProfessionalId(request.params.id))
 })
 
